ccf617992e698181afaab2f1d3b33f42a51cbd58
Manel BELHADJ <belhaj343@gmail.com>
2023-11-07 23:11:42 +0100

    Refactor GigCard component to use dynamic image and link URLs

--- client/src/components/gigCard/GigCard.jsx
+++ client/src/components/gigCard/GigCard.jsx
@@ -3,16 +3,24 @@ import "./GigCard.scss";
 import { Link } from "react-router-dom";
 
 const GigCard = ({ item }) => {
+  const imageUrl = './img/offers/' + item.image;
+  const profileImgUrl = './img/profiles/' + item.traveler.id + '.jpg';
+  const linkTo = '/gig/' + item.id;
   return (
-    <Link to="/gig/123" className="link">
+    <Link to={linkTo} className="link" onClick={
+      () => {
+        console.log(item);
+        localStorage.setItem('gig', JSON.stringify(item));
+      }
+    }>
       <div className="gigCard">
-        <img src={item.img} alt="" />
+        <img src={imageUrl} alt="" />
         <div className="info">
           <div className="user">
-            <img src={item.pp} alt="" />
-            <span>{item.username}</span>
+            <img src={profileImgUrl} alt="" />
+            <span>{item.traveler.user.username}</span>
           </div>
-          <p>{item.desc}</p>
+          <p>{item.description}</p>
           <div className="star">
             <img src="./img/star.png" alt="" />
             <span>{item.star}</span>
@@ -20,11 +28,14 @@ const GigCard = ({ item }) => {
         </div>
         <hr />
         <div className="detail">
-          <img src="./img/heart.png" alt="" />
+          {/* <img src="./img/heart.png" alt="" /> */}
+          <div className="delivery">
+            <span>Vers {item.destination}</span>
+          </div>
           <div className="price">
             <span>STARTING AT</span>
             <h2>
-              $ {item.price}
+              TND {item.price}
               <sup>99</sup>
             </h2>
           </div>
