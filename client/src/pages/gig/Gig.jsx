58752da5aab6e04ca261628a45701291dd06f8b8
Manel BELHADJ <belhaj343@gmail.com>
2023-11-07 23:28:26 +0100

    Refactor Gig component and update offer details

--- client/src/pages/gig/Gig.jsx
+++ client/src/pages/gig/Gig.jsx
@@ -1,21 +1,26 @@
 import React from "react";
-import "./Gig.scss";
 import { Slider } from "infinite-react-carousel/lib";
+import "./Gig.scss";
 
 function Gig() {
+  const item = JSON.parse(localStorage.getItem('gig'));
+  console.log(item);
+  const imageUrl = '/img/offers/' + item.image;
+  const profileImgUrl = '/img/profiles/' + item.traveler.id + '.jpg';
+  const dateFormatted = new Date(item.date).toLocaleDateString();
   return (
     <div className="gig">
       <div className="container">
         <div className="left">
-          <span className="breadcrumbs">Liverr Graphics & Design </span>
-          <h1>I will create ai generated art for you</h1>
+          <span className="breadcrumbs"> Transportation offer </span>
+          <h1>{item.title}</h1>
           <div className="user">
             <img
               className="pp"
-              src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+              src={profileImgUrl}
               alt=""
             />
-            <span>Anna Bell</span>
+            <span>{item.traveler.user.username}</span>
             <div className="stars">
               <img src="/img/star.png" alt="" />
               <img src="/img/star.png" alt="" />
@@ -26,44 +31,21 @@ function Gig() {
             </div>
           </div>
           <Slider slidesToShow={1} arrowsScroll={1} className="slider">
-            <img
-              src="https://images.pexels.com/photos/1074535/pexels-photo-1074535.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <img
-              src="https://images.pexels.com/photos/1462935/pexels-photo-1462935.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
-            <img
-              src="https://images.pexels.com/photos/1054777/pexels-photo-1054777.jpeg?auto=compress&cs=tinysrgb&w=1600"
-              alt=""
-            />
+            <img src={imageUrl} alt="" />
           </Slider>
           <h2>About This Gig</h2>
           <p>
-            I use an AI program to create images based on text prompts. This
-            means I can help you to create a vision you have through a textual
-            description of your scene without requiring any reference images.
-            Some things I've found it often excels at are: Character portraits
-            (E.g. a picture to go with your DnD character) Landscapes (E.g.
-            wallpapers, illustrations to compliment a story) Logos (E.g. Esports
-            team, business, profile picture) You can be as vague or as
-            descriptive as you want. Being more vague will allow the AI to be
-            more creative which can sometimes result in some amazing images. You
-            can also be incredibly precise if you have a clear image of what you
-            want in mind. All of the images I create are original and will be
-            found nowhere else. If you have any questions you're more than
-            welcome to send me a message.
+           {item.description}
           </p>
           <div className="seller">
             <h2>About The Seller</h2>
             <div className="user">
               <img
-                src="https://images.pexels.com/photos/720327/pexels-photo-720327.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                src={profileImgUrl}
                 alt=""
               />
               <div className="info">
-                <span>Anna Bell</span>
+                <span>{item.traveler.user.username}</span>
                 <div className="stars">
                   <img src="/img/star.png" alt="" />
                   <img src="/img/star.png" alt="" />
@@ -238,12 +220,11 @@ function Gig() {
         </div>
         <div className="right">
           <div className="price">
-            <h3>1 AI generated image</h3>
-            <h2>$ 59.99</h2>
+            <h3>{item.title}</h3>
+            <h2> TND {item.price}</h2>
           </div>
           <p>
-            I will create a unique high quality AI generated image based on a
-            description that you give me
+            {item.description}
           </p>
           <div className="details">
             <div className="item">
@@ -252,26 +233,19 @@ function Gig() {
             </div>
             <div className="item">
               <img src="/img/recycle.png" alt="" />
-              <span>3 Revisions</span>
+              <span> {dateFormatted} </span>
             </div>
           </div>
           <div className="features">
             <div className="item">
               <img src="/img/greencheck.png" alt="" />
-              <span>Prompt writing</span>
+              <span>Arrivee {item.destination}</span>
             </div>
             <div className="item">
               <img src="/img/greencheck.png" alt="" />
-              <span>Artwork delivery</span>
+              <span>Depart {item.depart}</span>
             </div>
-            <div className="item">
-              <img src="/img/greencheck.png" alt="" />
-              <span>Image upscaling</span>
-            </div>
-            <div className="item">
-              <img src="/img/greencheck.png" alt="" />
-              <span>Additional design</span>
-            </div>
+          
           </div>
           <button>Continue</button>
         </div>
