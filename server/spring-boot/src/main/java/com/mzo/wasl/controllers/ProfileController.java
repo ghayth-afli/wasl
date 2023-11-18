021286aa6b636d9f176e71681890a1913b3f335e
Manel BELHADJ <belhaj343@gmail.com>
2023-11-18 17:30:32 +0100

    add CORS support to ProfileController

--- server/spring-boot/src/main/java/com/mzo/wasl/controllers/ProfileController.java
+++ server/spring-boot/src/main/java/com/mzo/wasl/controllers/ProfileController.java
@@ -13,27 +13,30 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RequestMapping("/api")
 public class ProfileController {
     @Autowired
     private ProfileRepository profileRepository;
+
     @GetMapping("/myprofile")
     @PreAuthorize("hasRole('REGULAR')")
     public ResponseEntity<?> getProfile() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetailsImpl user = (UserDetailsImpl) authentication.getPrincipal();
-        Profile profile =profileRepository.findByUserId(user.getId());
+        Profile profile = profileRepository.findByUserId(user.getId());
         return ResponseEntity.ok(profile);
-        //return ResponseEntity.ok(new ProfileResponse(profile.getId(),profile.getFirstName(),profile.getLastName(),profile.getBio(),profile.getCountry(),profile.getCity(),profile.getPhoneNumber(),profile.getLanguage() ,profile.getImage()));
+        // return ResponseEntity.ok(new
+        // ProfileResponse(profile.getId(),profile.getFirstName(),profile.getLastName(),profile.getBio(),profile.getCountry(),profile.getCity(),profile.getPhoneNumber(),profile.getLanguage()
+        // ,profile.getImage()));
     }
-
 
     @PutMapping("/myprofile")
     @PreAuthorize("hasRole('REGULAR')")
     public ResponseEntity<?> UpdateProfile(@RequestBody ProfileRequest profileRequest) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         UserDetailsImpl user = (UserDetailsImpl) authentication.getPrincipal();
-        Profile profile =profileRepository.findByUserId(user.getId());
+        Profile profile = profileRepository.findByUserId(user.getId());
         profile.setFirstName(profileRequest.getFirstName());
         profile.setLastName(profileRequest.getLastName());
         profile.setBio(profileRequest.getBio());
@@ -43,7 +46,9 @@ public class ProfileController {
         profile.setLanguage(profileRequest.getLanguage());
         profile.setImage(profileRequest.getImage());
         profileRepository.save(profile);
-        return ResponseEntity.ok(new ProfileResponse(profile.getId(),profile.getFirstName(),profile.getLastName(),profile.getBio(),profile.getCountry(),profile.getCity(),profile.getPhoneNumber(),profile.getLanguage() ,profile.getImage()));
+        return ResponseEntity.ok(new ProfileResponse(profile.getId(), profile.getFirstName(), profile.getLastName(),
+                profile.getBio(), profile.getCountry(), profile.getCity(), profile.getPhoneNumber(),
+                profile.getLanguage(), profile.getImage()));
     }
 
 }
