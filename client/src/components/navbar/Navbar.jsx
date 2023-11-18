c56e32daf293dbd619990a7d5afef16be968ac0c
Manel BELHADJ <belhaj343@gmail.com>
2023-11-18 17:31:15 +0100

    Add user authentication and profile functionality

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect, useState } from "react";
 import { Link, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
+import { hosts } from "../../const.js";
 import "./Navbar.scss";
 
 function Navbar() {
@@ -8,8 +9,9 @@ function Navbar() {
   const [active, setActive] = useState(false);
   const [open, setOpen] = useState(false);
   const { pathname } = useLocation();
-
+  const [user, setUser] = useState(null);
   const token = localStorage.getItem("token") || null;
+  console.log("token", token);
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -18,6 +20,7 @@ function Navbar() {
   const handleLogout = () => {
     console.log("user logout logic");
     localStorage.removeItem("token");
+    localStorage.removeItem("gig");
     navigate("/");
 
   };
@@ -28,20 +31,39 @@ function Navbar() {
       window.removeEventListener("scroll", isActive);
     };
   }, []);
-  
-  // const currentUser = null;
-  var currentUser
+
+ 
+
+  var currentUser;
+
   if (token) {
-     currentUser = {
-      id: 1,
-      username: "Anna",
-      isSeller: true,
-    };
+    useEffect(() => {
+      fetch(`${hosts.backend}/api/myprofile`, {
+        method: "GET",
+        headers: {
+          "Content-Type": "Application/json ; charset=UTF-8",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+      })
+        .then((res) => {
+          console.log("res", res);
+          return res.json();
+        })
+        .then((data) => {
+          console.log("data", data);
+          setUser(data);
+        })
+        .catch((err) => {
+          console.log("err", err);
+          setUser(null);
+        });
+    }, []);
+     currentUser = user;
   } else {
     console.log("token does not exist");
     currentUser = null;
   }
-  
+    
 
   return (
     <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
@@ -56,20 +78,21 @@ function Navbar() {
           <Link className="link" to="/gigs">
             <span>Explore</span>
           </Link>
-          {!currentUser?.isSeller && <span>Become a Seller</span>}
+          {!currentUser?.traveler && <span>Become a Traveller</span>}
+          {currentUser?.traveler && <span>Become a Sender</span>}
           {currentUser ? (
             <div className="user" onClick={() => setOpen(!open)}>
               <img
                 src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
                 alt=""
               />
-              <span>{currentUser?.username}</span>
+              <span>{currentUser?.user?.username}</span>
               {open && (
                 <div className="options">
                   <Link className="link" to="/profile">
                     Profile
                   </Link>
-                  {currentUser.isSeller && (
+                  {currentUser.traveler && (
                     <>
                       <Link className="link" to="/mygigs">
                         Gigs
