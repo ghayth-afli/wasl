522a04e8bec6db3f908798b0f1b5014c1ca0ab58
Manel BELHADJ <belhaj343@gmail.com>
2023-11-20 10:29:59 +0100

    Add switchRole function

--- client/src/components/navbar/Navbar.jsx
+++ client/src/components/navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import Button from '@mui/material/Button';
 import { Link, useLocation } from "react-router-dom";
 import { useNavigate } from "react-router-dom";
 import { hosts } from "../../const.js";
@@ -10,8 +11,8 @@ function Navbar() {
   const [open, setOpen] = useState(false);
   const { pathname } = useLocation();
   const [user, setUser] = useState(null);
+
   const token = localStorage.getItem("token") || null;
-  console.log("token", token);
 
   const isActive = () => {
     window.scrollY > 0 ? setActive(true) : setActive(false);
@@ -33,37 +34,57 @@ function Navbar() {
   }, []);
 
  
-
+  function switchRole() {
+    fetch(`${hosts.backend}/api/switch`, {
+      method: "PUT",
+      headers: {
+        "Content-Type": "Application/json ; charset=UTF-8",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+      .then((res) => {
+        console.log("res", res);
+        return res.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+        setUser(data);
+      })
+      .catch((err) => {
+        console.log("err", err);
+        // setUser(null);
+      });
+  }
   var currentUser;
 
-  if (token) {
+  
     useEffect(() => {
       fetch(`${hosts.backend}/api/myprofile`, {
         method: "GET",
         headers: {
           "Content-Type": "Application/json ; charset=UTF-8",
-          Authorization: "Bearer " + localStorage.getItem("token"),
+          Authorization: "Bearer " + localStorage.getItem("token") || null,
         },
       })
         .then((res) => {
-          console.log("res", res);
           return res.json();
         })
         .then((data) => {
-          console.log("data", data);
           setUser(data);
+          console.log("data", data);
         })
         .catch((err) => {
-          console.log("err", err);
           setUser(null);
+          currentUser = null;
         });
     }, []);
-     currentUser = user;
-  } else {
-    console.log("token does not exist");
-    currentUser = null;
-  }
-    
+
+    if (token){
+      currentUser = user;
+      var profileImgUrl = './img/profiles/' + currentUser?.id + '.jpg' || './img/profiles/0.jpg';
+      console.log("token exists", profileImgUrl);
+    }
+
 
   return (
     <div className={active || pathname !== "/" ? "navbar active" : "navbar"}>
@@ -78,13 +99,13 @@ function Navbar() {
           <Link className="link" to="/gigs">
             <span>Explore</span>
           </Link>
-          {!currentUser?.traveler && <span>Become a Traveller</span>}
-          {currentUser?.traveler && <span>Become a Sender</span>}
+          {currentUser && !currentUser?.traveler && <Button  onClick={switchRole}>Become a Traveller</Button >}
+          {currentUser?.traveler && <Button >Become a Sender</Button >}
           {currentUser ? (
             <div className="user" onClick={() => setOpen(!open)}>
               <img
-                src="https://images.pexels.com/photos/1115697/pexels-photo-1115697.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                alt=""
+                src={profileImgUrl }
+                alt="user image"
               />
               <span>{currentUser?.user?.username}</span>
               {open && (
