23650e6eeb0bf56ec3da959484dff1f1c65f17e5
Manel BELHADJ <belhaj343@gmail.com>
2023-11-21 17:23:46 +0100

    Add profile update

--- client/src/components/profile/Profile.jsx
+++ client/src/components/profile/Profile.jsx
@@ -31,8 +31,63 @@ import InsertDriveFileRoundedIcon from "@mui/icons-material/InsertDriveFileRound
 import EditRoundedIcon from "@mui/icons-material/EditRounded";
 
 import CountrySelector from "./ContrySelector.jsx";
+import { hosts } from "../../const.js";
 
 export default function MyProfile() {
+  const [user, setUser] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch(`${hosts.backend}/api/myprofile`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "Application/json ; charset=UTF-8",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+        setUser(data);
+      })
+      .catch((err) => {
+        setUser(null);
+      });
+  }, []);
+
+  const handleChange = (event) => {
+    console.log("event", event.target.name, "dfsdf", event.target.value);
+    setUser({
+      ...user,
+      [event.target.name]: event.target.value,
+    });
+  };
+
+  var profileImgUrl =
+    "./img/profiles/" + user?.id + ".jpg" || "./img/profiles/1.jpg";
+
+  const updateProfile = async (event) => {
+    event.preventDefault();
+    console.log("user new", user);
+    try {
+      const response = await fetch(`${hosts.backend}/api/myprofile`, {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          Authorization: "Bearer " + localStorage.getItem("token"),
+        },
+        body: JSON.stringify(user),
+      });
+      console.log("response", response);
+      const data = await response.json();
+      console.log("data", data);
+      setUser(data);
+    } catch (err) {
+      console.log("err", err);
+    }
+  };
+
   return (
     <Box
       sx={{
@@ -91,10 +146,7 @@ export default function MyProfile() {
               Overview
             </Tab>
             <Tab sx={{ borderRadius: "6px 6px 0 0" }} indicatorInset value={1}>
-              Settings
-            </Tab>
-            <Tab sx={{ borderRadius: "6px 6px 0 0" }} indicatorInset value={2}>
-              Billing
+              Dashboard
             </Tab>
           </TabList>
         </Tabs>
@@ -136,8 +188,8 @@ export default function MyProfile() {
                 sx={{ flex: 1, minWidth: 120, borderRadius: "100%" }}
               >
                 <img
-                  src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
-                  srcSet="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286&dpr=2 2x"
+                  src={profileImgUrl || "./img/profiles/0.png"}
+                  srcSet={profileImgUrl}
                   loading="lazy"
                   alt=""
                 />
@@ -172,18 +224,41 @@ export default function MyProfile() {
                     gap: 2,
                   }}
                 >
-                  <Input size="sm" placeholder="First name" />
+                  <Input
+                    size="sm"
+                    placeholder="User name"
+                    sx={{ flexGrow: 1 }}
+                    value={user?.userName}
+                    onChange={handleChange}
+                    name="userName"
+                  />
+                  <Input
+                    size="sm"
+                    placeholder="First name"
+                    sx={{ flexGrow: 1 }}
+                    value={user?.firstName}
+                    onChange={handleChange}
+                    name="firstName"
+                  />
                   <Input
                     size="sm"
                     placeholder="Last name"
                     sx={{ flexGrow: 1 }}
+                    value={user?.lastName}
+                    onChange={handleChange}
+                    name="lastName"
                   />
                 </FormControl>
               </Stack>
               <Stack direction="row" spacing={2}>
                 <FormControl>
-                  <FormLabel>Role</FormLabel>
-                  <Input size="sm" defaultValue="UI Developer" />
+                  <FormLabel>Language</FormLabel>
+                  <Input
+                    size="sm"
+                    value={user?.language || "English"}
+                    onChange={handleChange}
+                    name="language"
+                  />
                 </FormControl>
                 <FormControl sx={{ flexGrow: 1 }}>
                   <FormLabel>Email</FormLabel>
@@ -194,140 +269,24 @@ export default function MyProfile() {
                     placeholder="email"
                     defaultValue="siriwatk@test.com"
                     sx={{ flexGrow: 1 }}
+                    value={user?.user?.email || ""}
+                    onChange={handleChange}
+                    name="email"
                   />
                 </FormControl>
               </Stack>
               <div>
                 <CountrySelector />
               </div>
-              <div>
-                <FormControl sx={{ display: { sm: "contents" } }}>
-                  <FormLabel>Timezone</FormLabel>
-                  <Select
-                    size="sm"
-                    startDecorator={<AccessTimeFilledRoundedIcon />}
-                    defaultValue="1"
-                  >
-                    <Option value="1">
-                      Indochina Time (Bangkok){" "}
-                      <Typography textColor="text.tertiary" ml={0.5}>
-                        — GMT+07:00
-                      </Typography>
-                    </Option>
-                    <Option value="2">
-                      Indochina Time (Ho Chi Minh City){" "}
-                      <Typography textColor="text.tertiary" ml={0.5}>
-                        — GMT+07:00
-                      </Typography>
-                    </Option>
-                  </Select>
-                </FormControl>
-              </div>
             </Stack>
           </Stack>
-          <Stack
-            direction="column"
-            spacing={2}
-            sx={{ display: { xs: "flex", md: "none" }, my: 1 }}
-          >
-            <Stack direction="row" spacing={2}>
-              <Stack direction="column" spacing={1}>
-                <AspectRatio
-                  ratio="1"
-                  maxHeight={108}
-                  sx={{ flex: 1, minWidth: 108, borderRadius: "100%" }}
-                >
-                  <img
-                    src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
-                    srcSet="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286&dpr=2 2x"
-                    loading="lazy"
-                    alt=""
-                  />
-                </AspectRatio>
-                <IconButton
-                  aria-label="upload new picture"
-                  size="sm"
-                  variant="outlined"
-                  color="neutral"
-                  sx={{
-                    bgcolor: "background.body",
-                    position: "absolute",
-                    zIndex: 2,
-                    borderRadius: "50%",
-                    left: 85,
-                    top: 180,
-                    boxShadow: "sm",
-                  }}
-                >
-                  <EditRoundedIcon />
-                </IconButton>
-              </Stack>
-              <Stack spacing={1} sx={{ flexGrow: 1 }}>
-                <FormLabel>Name</FormLabel>
-                <FormControl
-                  sx={{
-                    display: {
-                      sm: "flex-column",
-                      md: "flex-row",
-                    },
-                    gap: 2,
-                  }}
-                >
-                  <Input size="sm" placeholder="First name" />
-                  <Input size="sm" placeholder="Last name" />
-                </FormControl>
-              </Stack>
-            </Stack>
 
-            <FormControl>
-              <FormLabel>Role</FormLabel>
-              <Input size="sm" defaultValue="UI Developer" />
-            </FormControl>
-            <FormControl sx={{ flexGrow: 1 }}>
-              <FormLabel>Email</FormLabel>
-              <Input
-                size="sm"
-                type="email"
-                startDecorator={<EmailRoundedIcon />}
-                placeholder="email"
-                defaultValue="siriwatk@test.com"
-                sx={{ flexGrow: 1 }}
-              />
-            </FormControl>
-
-            <div>
-              <CountrySelector />
-            </div>
-            <div>
-              <FormControl sx={{ display: { sm: "contents" } }}>
-                <FormLabel>Timezone</FormLabel>
-                <Select
-                  size="sm"
-                  startDecorator={<AccessTimeFilledRoundedIcon />}
-                  defaultValue="1"
-                >
-                  <Option value="1">
-                    Indochina Time (Bangkok){" "}
-                    <Typography textColor="text.tertiary" ml={0.5}>
-                      — GMT+07:00
-                    </Typography>
-                  </Option>
-                  <Option value="2">
-                    Indochina Time (Ho Chi Minh City){" "}
-                    <Typography textColor="text.tertiary" ml={0.5}>
-                      — GMT+07:00
-                    </Typography>
-                  </Option>
-                </Select>
-              </FormControl>
-            </div>
-          </Stack>
           <CardOverflow sx={{ borderTop: "1px solid", borderColor: "divider" }}>
             <CardActions sx={{ alignSelf: "flex-end", pt: 2 }}>
               <Button size="sm" variant="outlined" color="neutral">
                 Cancel
               </Button>
-              <Button size="sm" variant="solid">
+              <Button size="sm" variant="solid" onClick={updateProfile}>
                 Save
               </Button>
             </CardActions>
@@ -346,7 +305,10 @@ export default function MyProfile() {
               size="sm"
               minRows={4}
               sx={{ mt: 1.5 }}
-              defaultValue="I'm a software developer based in Bangkok, Thailand. My goal is to solve UI problems with neat CSS without using too much JavaScript."
+              placeholder="Write a short introduction to be displayed on your profile"
+              value={user?.bio || ""}
+              onChange={handleChange}
+              name="bio"
             />
             <FormHelperText sx={{ mt: 0.75, fontSize: "xs" }}>
               275 characters left
@@ -357,7 +319,7 @@ export default function MyProfile() {
               <Button size="sm" variant="outlined" color="neutral">
                 Cancel
               </Button>
-              <Button size="sm" variant="solid">
+              <Button size="sm" variant="solid" onClick={updateProfile}>
                 Save
               </Button>
             </CardActions>
