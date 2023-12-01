a325c10d57034daed1c36f1eb58482f3c1bd32c0
Manel BELHADJ <belhaj343@gmail.com>
2023-12-01 12:27:59 +0100

    Major auth ui updates

--- client/src/pages/login/Login.jsx
+++ client/src/pages/login/Login.jsx
@@ -1,48 +1,59 @@
-// import React, { useState } from "react";
-import "./Login.scss";
-// import newRequest from "../../utils/newRequest";
-import { useNavigate } from "react-router-dom";
-
 import * as React from "react";
-import Avatar from "@mui/material/Avatar";
-import Button from "@mui/material/Button";
-import CssBaseline from "@mui/material/CssBaseline";
-import TextField from "@mui/material/TextField";
-import FormControlLabel from "@mui/material/FormControlLabel";
-import Checkbox from "@mui/material/Checkbox";
-import Link from "@mui/material/Link";
+import { CssVarsProvider, useColorScheme } from "@mui/joy/styles";
+import GlobalStyles from "@mui/joy/GlobalStyles";
+import CssBaseline from "@mui/joy/CssBaseline";
+import Box from "@mui/joy/Box";
+import Button from "@mui/joy/Button";
+import Checkbox from "@mui/joy/Checkbox";
+import FormControl from "@mui/joy/FormControl";
+import FormLabel, { formLabelClasses } from "@mui/joy/FormLabel";
+import IconButton from "@mui/joy/IconButton";
+import Link from "@mui/joy/Link";
+import Input from "@mui/joy/Input";
+import Typography from "@mui/joy/Typography";
+import Stack from "@mui/joy/Stack";
+import DarkModeRoundedIcon from "@mui/icons-material/DarkModeRounded";
+import LightModeRoundedIcon from "@mui/icons-material/LightModeRounded";
+import { useNavigate } from "react-router-dom";
 import Grid from "@mui/material/Grid";
-import Box from "@mui/material/Box";
-import LockOutlinedIcon from "@mui/icons-material/LockOutlined";
-import Typography from "@mui/material/Typography";
-import Container from "@mui/material/Container";
-import { createTheme, ThemeProvider } from "@mui/material/styles";
 import { hosts } from "../../const.js";
 
-function Copyright(props) {
+function ColorSchemeToggle(props) {
+  const { onClick, ...other } = props;
+  const { mode, setMode } = useColorScheme();
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    return <IconButton size="sm" variant="outlined" color="neutral" disabled />;
+  }
   return (
-    <Typography
-      variant="body2"
-      color="text.secondary"
-      align="center"
-      {...props}
+    <IconButton
+      id="toggle-mode"
+      size="sm"
+      variant="outlined"
+      color="neutral"
+      aria-label="toggle light/dark mode"
+      {...other}
+      onClick={(event) => {
+        if (mode === "light") {
+          setMode("dark");
+        } else {
+          setMode("light");
+        }
+        onClick?.(event);
+      }}
     >
-      {"Copyright © "}
-      <Link color="inherit" href="">
-        WASSL
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
+      {mode === "light" ? <DarkModeRoundedIcon /> : <LightModeRoundedIcon />}
+    </IconButton>
   );
 }
 
-const defaultTheme = createTheme();
-
-function Login() {
+export default function Login() {
   const navigate = useNavigate();
   const [error, setError] = React.useState(null);
-
+  
   const handleSubmit = async (event) => {
     event.preventDefault();
     const data = new FormData(event.currentTarget);
@@ -51,7 +62,6 @@ function Login() {
       password: data.get("password"),
     });
     try {
-      console.log("user login logic");
       fetch(`${hosts.backend}/api/auth/signin`, {
         method: "POST",
         body: JSON.stringify({
@@ -79,88 +89,169 @@ function Login() {
       setError(err.response.data);
     }
   };
-
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Container component="main" maxWidth="xs">
-        <CssBaseline />
+    <CssVarsProvider defaultMode="light" disableTransitionOnChange>
+      <CssBaseline />
+      <GlobalStyles
+        styles={{
+          ":root": {
+            "--Collapsed-breakpoint": "769px", // form will stretch when viewport is below `769px`
+            "--Cover-width": "50vw", // must be `vw` only
+            "--Form-maxWidth": "800px",
+            "--Transition-duration": "0.4s", // set to `none` to disable transition
+          },
+        }}
+      />
+      <Box
+        sx={(theme) => ({
+          width:
+            "clamp(100vw - var(--Cover-width), (var(--Collapsed-breakpoint) - 100vw) * 999, 100vw)",
+          transition: "width var(--Transition-duration)",
+          transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
+          position: "relative",
+          zIndex: 1,
+          display: "flex",
+          justifyContent: "flex-end",
+          backdropFilter: "blur(12px)",
+          backgroundColor: "rgba(255 255 255 / 0.2)",
+          [theme.getColorSchemeSelector("dark")]: {
+            backgroundColor: "rgba(19 19 24 / 0.4)",
+          },
+        })}
+      >
         <Box
           sx={{
-            marginTop: 8,
             display: "flex",
             flexDirection: "column",
-            alignItems: "center",
+            minHeight: "100dvh",
+            width:
+              "clamp(var(--Form-maxWidth), (var(--Collapsed-breakpoint) - 100vw) * 999, 100%)",
+            maxWidth: "100%",
+            px: 2,
           }}
         >
-          <Avatar sx={{ m: 1, bgcolor: "primary.main" }}>
-            <LockOutlinedIcon />
-          </Avatar>
-          <Typography component="h1" variant="h5">
-            Sign in
-          </Typography>
           <Box
-            component="form"
-            onSubmit={handleSubmit}
-            noValidate
-            sx={{ mt: 1 }}
+            component="header"
+            sx={{
+              py: 3,
+              display: "flex",
+              alignItems: "left",
+              justifyContent: "space-between",
+            }}
           >
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              id="email"
-              label="Email Address"
-              name="email"
-              autoComplete="email"
-              autoFocus
-            />
-            <TextField
-              margin="normal"
-              required
-              fullWidth
-              name="password"
-              label="Password"
-              type="password"
-              id="password"
-              autoComplete="current-password"
-            />
-            <FormControlLabel
-              control={<Checkbox value="remember" color="primary" />}
-              label="Remember me"
-            />
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Sign In
-            </Button>
-            <Grid container>
-              <Grid item xs>
-                <Link to="/" variant="body2">
-                  Forgot password?
-                </Link>
-              </Grid>
-              <Grid item>
-                <Link href="/register" variant="body2">
-                  {"Don't have an account? Sign Up"}
-                </Link>
-              </Grid>
-            </Grid>
+            <Box sx={{ gap: 2, display: "flex", alignItems: "center" }}>
+            
+              <Typography level="title-lg">
+               <Link href="/">
+               <img src={"https://res.cloudinary.com/dlxlpazb7/image/upload/v1700931317/wasl/logo5_va4dzh.png"} width="80px" />
+               </Link>
+              </Typography>
+            </Box>
+            <ColorSchemeToggle />
+          </Box>
+          <Box
+            component="main"
+            sx={{
+              my: "auto",
+              py: 2,
+              pb: 5,
+              display: "flex",
+              flexDirection: "column",
+              gap: 2,
+              width: 400,
+              maxWidth: "100%",
+              mx: "auto",
+              borderRadius: "sm",
+              "& form": {
+                display: "flex",
+                flexDirection: "column",
+                gap: 2,
+              },
+              [`& .${formLabelClasses.asterisk}`]: {
+                visibility: "hidden",
+              },
+            }}
+          >
+            <Stack gap={4} sx={{ mb: 2 }}>
+              <Stack gap={1}>
+                <Typography level="h3">Sign in</Typography>
+                <Typography level="body-sm">
+                  New to wasl?{" "}
+                  <Link href="/register" level="title-sm" color="success">
+                    Sign up!
+                  </Link>
+                </Typography>
+              </Stack>
+            </Stack>
+
+            <Stack gap={4} sx={{ mt: 2 }}>
+              <form onSubmit={handleSubmit}>
+                <FormControl required>
+                  <FormLabel>Email</FormLabel>
+                  <Input type="email" name="email" />
+                </FormControl>
+                <FormControl required>
+                  <FormLabel>Password</FormLabel>
+                  <Input type="password" name="password" />
+                </FormControl>
+             
+                <Stack gap={4} sx={{ mt: 2 }}>
+                  <Box
+                    sx={{
+                      display: "flex",
+                      justifyContent: "space-between",
+                      alignItems: "center",
+                    }}
+                  >
+                    <Checkbox size="sm" label="Remember me" name="persistent" />
+                    <Link level="title-sm" href="#replace-with-a-link" color="success">
+                      Forgot your password?
+                    </Link>
+                  </Box>
+                  <Button type="submit" fullWidth color="success">
+                    Sign in
+                  </Button>
+                </Stack>
+              </form>
+              {error && (
+                <Grid item xs={12}>
+                  <Typography color="danger">
+                    {error}
+                  </Typography>
+                </Grid>
+              )}
+            </Stack>
+          </Box>
+          <Box component="footer" sx={{ py: 3 }}>
+            <Typography level="body-xs" textAlign="center">
+              © Wasl {new Date().getFullYear()}
+            </Typography>
           </Box>
         </Box>
-        {error && (
-          <Grid item xs={12}>
-            <Typography variant="body2" color="error">
-              {error}
-            </Typography>
-          </Grid>
-        )}
-        <Copyright sx={{ mt: 8, mb: 4 }} />
-      </Container>
-    </ThemeProvider>
+      </Box>
+      <Box
+        sx={(theme) => ({
+          height: "100%",
+          position: "fixed",
+          right: 0,
+          top: 0,
+          bottom: 0,
+          left: "clamp(0px, (100vw - var(--Collapsed-breakpoint)) * 999, 100vw - var(--Cover-width))",
+          transition:
+            "background-image var(--Transition-duration), left var(--Transition-duration) !important",
+          transitionDelay: "calc(var(--Transition-duration) + 0.1s)",
+          backgroundColor: "background.level1",
+          backgroundSize: "cover",
+          backgroundPosition: "center",
+          backgroundRepeat: "no-repeat",
+          backgroundImage:
+            "url(https://images.unsplash.com/photo-1527181152855-fc03fc7949c8?auto=format&w=1000&dpr=2)",
+          [theme.getColorSchemeSelector("dark")]: {
+            backgroundImage:
+              "url(https://images.unsplash.com/photo-1572072393749-3ca9c8ea0831?auto=format&w=1000&dpr=2)",
+          },
+        })}
+      />
+    </CssVarsProvider>
   );
 }
-
-export default Login;
