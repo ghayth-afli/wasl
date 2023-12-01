527960c88e07921a4f6fc0eaa8ba64ea840f1760
Manel BELHADJ <belhaj343@gmail.com>
2023-12-01 18:35:46 +0100

    Fix formatting and error handling in MyGigs component

--- client/src/pages/myGigs/MyGigs.jsx
+++ client/src/pages/myGigs/MyGigs.jsx
@@ -1,16 +1,36 @@
 import React, { useEffect } from "react";
-import { Link ,useNavigate} from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
 import "./MyGigs.scss";
 import { hosts } from "../../const";
 
 function MyGigs() {
   const navigate = useNavigate();
-  const currentUser = {
-    id: 1,
-    username: "Anna",
-    isSeller: true,
-  };
   const [offers, setOffers] = React.useState([]);
+  const [currentUser, setUser] = React.useState(null);
+
+  useEffect(() => {
+    fetch(`${hosts.backend}/api/myprofile`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "Application/json ; charset=UTF-8",
+        Authorization: "Bearer " + localStorage.getItem("token"),
+      },
+    })
+      .then((res) => {
+        return res.json();
+      })
+      .then((data) => {
+        if (data.error) {
+          throw new Error(data.error);
+        }
+        setUser(data);
+      })
+      .catch((err) => {
+        navigate("/login");
+        setUser(null);
+      });
+  }, []);
 
   const offersFetching = () => {
     fetch(`${hosts.backend}/api/offers`, {
@@ -24,6 +44,19 @@ function MyGigs() {
       })
       .then((data) => {
         setOffers(data);
+      })
+      .catch((err) => {
+        console.log(err);
+        toast.error("Couldnt fetch offers- check again later", {
+          position: "bottom-right",
+          autoClose: 10000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
       });
   };
   useEffect(() => {
@@ -44,23 +77,33 @@ function MyGigs() {
       })
       .then((data) => {
         console.log("data in deleting", data);
+        toast.success("🦄 Offer deleted!", {
+          position: "bottom-right",
+          autoClose: 10000,
+          hideProgressBar: false,
+          closeOnClick: true,
+          pauseOnHover: true,
+          draggable: true,
+          progress: undefined,
+          theme: "dark",
+        });
       })
       .then(() => {
         offersFetching();
       });
   };
-  const handelUpdate=(event ,id)=>{
+  const handelUpdate = (event, id) => {
     event.preventDefault();
-    const link = "/edit/"+id
-    navigate(link)
-  }
+    const link = "/edit/" + id;
+    navigate(link);
+  };
 
   return (
     <div className="myGigs">
       <div className="container">
         <div className="title">
-          <h1>{currentUser.isSeller ? "Gigs" : "Orders"}</h1>
-          {currentUser.isSeller && (
+          <h1>{currentUser?.traveler ? "Gigs" : "Orders"}</h1>
+          {currentUser?.traveler && (
             <Link to="/add">
               <button>Add New Gig</button>
             </Link>
@@ -77,11 +120,7 @@ function MyGigs() {
           {offers.map((offer) => (
             <tr key={offer.id}>
               <td>
-                <img
-                  className="image"
-                  src={`./img/offers/${offer.image}`}
-                  alt=""
-                />
+                <img className="image" src={`${offer.image}`} alt="" />
               </td>
               <td>{offer.title}</td>
               <td>
@@ -98,17 +137,12 @@ function MyGigs() {
                 />
               </td>
               <td>
-              
-            
-                
                 <img
-                   className="edit"
-                   src="./img/edit.png"
-                   alt=""
-                   onClick={(event)=>handelUpdate(event,offer.id)}
-                   ></img>
-                  
-                    
+                  className="edit"
+                  src="./img/edit.png"
+                  alt="edit"
+                  onClick={(event) => handelUpdate(event, offer.id)}
+                ></img>
               </td>
             </tr>
           ))}
