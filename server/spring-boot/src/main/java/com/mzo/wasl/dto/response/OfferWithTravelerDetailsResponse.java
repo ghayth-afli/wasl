55556cd24d6ccf47c25abcaef406c2d35f024410
Afli Mohamed Ghayth <40703798+ghayth-afli@users.noreply.github.com>
2023-12-02 21:44:56 +0100

    offer with about the traveler

---
+++ server/spring-boot/src/main/java/com/mzo/wasl/dto/response/OfferWithTravelerDetailsResponse.java
@@ -0,0 +1,33 @@
+package com.mzo.wasl.dto.response;
+
+import jakarta.persistence.Column;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Time;
+import java.util.Date;
+@AllArgsConstructor
+@Getter
+@Setter
+public class OfferWithTravelerDetailsResponse {
+    private Long id;
+    private String title;
+    private String description;
+    private String depart;
+    private String destination;
+    private Date date;
+    private Time time;
+    private Double price;
+    private Double capacity;
+    private Double remainingCapacity;
+    private String image;
+    private Long travelerId;
+    private String travelerName;
+    private String travelerEmail;
+    private String travelerPhone;
+    private String travelerCountry;
+    private String travelerLanguage;
+    private String travelerBio;
+}
