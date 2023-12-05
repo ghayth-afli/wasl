beaa87ec06ad178269ea20814414d9cdb9737c6c
Manel BELHADJ <belhaj343@gmail.com>
2023-12-05 22:57:56 +0100

    Fix typo in gig

--- client/src/pages/gig/Gig.jsx
+++ client/src/pages/gig/Gig.jsx
@@ -133,175 +133,174 @@ function Gig() {
                   <span className="title">From</span>
                   <span className="desc">{item.travelerCountry}</span>
 
+                  <div className="item">
+                    <span className="title">Languages</span>
+                    <span className="desc">English</span>
+                  </div>
+                </div>
+                <hr />
 
-                <div className="item">
-                  <span className="title">Languages</span>
-                  <span className="desc">English</span>
+                <p>{"About: " + item.travelerBio}</p>
+              </div>
+            </div>
+            <div className="reviews">
+              <h2>Reviews</h2>
+              <div className="item">
+                <div className="user">
+                  <img
+                    className="pp"
+                    src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="info">
+                    <span>Garner David</span>
+                    <div className="country">
+                      <img
+                        src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
+                        alt=""
+                      />
+                      <span>United States</span>
+                    </div>
+                  </div>
+                </div>
+                <div className="stars">
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <span>5</span>
+                </div>
+                <p>
+                  I just want to say that art_with_ai was the first, and after
+                  this, the only artist Ill be using on Fiverr. Communication
+                  was amazing, each and every day he sent me images that I was
+                  free to request changes to. They listened, understood, and
+                  delivered above and beyond my expectations. I absolutely
+                  recommend this gig, and know already that Ill be using it
+                  again very very soon
+                </p>
+                <div className="helpful">
+                  <span>Helpful?</span>
+                  <img src="/img/like.png" alt="" />
+                  <span>Yes</span>
+                  <img src="/img/dislike.png" alt="" />
+                  <span>No</span>
                 </div>
               </div>
               <hr />
-
-              <p>{"About: " + item.travelerBio}</p>
-
-
-            </div>
-          </div>
-          <div className="reviews">
-            <h2>Reviews</h2>
-            <div className="item">
-              <div className="user">
-                <img
-                  className="pp"
-                  src="https://images.pexels.com/photos/839586/pexels-photo-839586.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <div className="info">
-                  <span>Garner David</span>
-                  <div className="country">
-                    <img
-                      src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
-                      alt=""
-                    />
-                    <span>United States</span>
+              <div className="item">
+                <div className="user">
+                  <img
+                    className="pp"
+                    src="https://images.pexels.com/photos/4124367/pexels-photo-4124367.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="info">
+                    <span>Sidney Owen</span>
+                    <div className="country">
+                      <img
+                        src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1e9-1f1ea.png"
+                        alt=""
+                      />
+                      <span>Germany</span>
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="stars">
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <span>5</span>
-              </div>
-              <p>
-                I just want to say that art_with_ai was the first, and after
-                this, the only artist Ill be using on Fiverr. Communication was
-                amazing, each and every day he sent me images that I was free to
-                request changes to. They listened, understood, and delivered
-                above and beyond my expectations. I absolutely recommend this
-                gig, and know already that Ill be using it again very very soon
-              </p>
-              <div className="helpful">
-                <span>Helpful?</span>
-                <img src="/img/like.png" alt="" />
-                <span>Yes</span>
-                <img src="/img/dislike.png" alt="" />
-                <span>No</span>
-              </div>
-            </div>
-            <hr />
-            <div className="item">
-              <div className="user">
-                <img
-                  className="pp"
-                  src="https://images.pexels.com/photos/4124367/pexels-photo-4124367.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <div className="info">
-                  <span>Sidney Owen</span>
-                  <div className="country">
-                    <img
-                      src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1e9-1f1ea.png"
-                      alt=""
-                    />
-                    <span>Germany</span>
-                  </div>
+                <div className="stars">
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <span>5</span>
+                </div>
+                <p>
+                  The designer took my photo for my book cover to the next
+                  level! Professionalism and ease of working with designer along
+                  with punctuality is above industry standards!! Whatever your
+                  project is, you need this designer!
+                </p>
+                <div className="helpful">
+                  <span>Helpful?</span>
+                  <img src="/img/like.png" alt="" />
+                  <span>Yes</span>
+                  <img src="/img/dislike.png" alt="" />
+                  <span>No</span>
                 </div>
               </div>
-              <div className="stars">
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <span>5</span>
-              </div>
-              <p>
-                The designer took my photo for my book cover to the next level!
-                Professionalism and ease of working with designer along with
-                punctuality is above industry standards!! Whatever your project
-                is, you need this designer!
-              </p>
-              <div className="helpful">
-                <span>Helpful?</span>
-                <img src="/img/like.png" alt="" />
-                <span>Yes</span>
-                <img src="/img/dislike.png" alt="" />
-                <span>No</span>
-              </div>
-            </div>
-            <hr />
-            <div className="item">
-              <div className="user">
-                <img
-                  className="pp"
-                  src="https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&w=1600"
-                  alt=""
-                />
-                <div className="info">
-                  <span>Lyle Giles </span>
-                  <div className="country">
-                    <img
-                      src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
-                      alt=""
-                    />
-                    <span>United States</span>
+              <hr />
+              <div className="item">
+                <div className="user">
+                  <img
+                    className="pp"
+                    src="https://images.pexels.com/photos/842980/pexels-photo-842980.jpeg?auto=compress&cs=tinysrgb&w=1600"
+                    alt=""
+                  />
+                  <div className="info">
+                    <span>Lyle Giles </span>
+                    <div className="country">
+                      <img
+                        src="https://fiverr-dev-res.cloudinary.com/general_assets/flags/1f1fa-1f1f8.png"
+                        alt=""
+                      />
+                      <span>United States</span>
+                    </div>
                   </div>
                 </div>
+                <div className="stars">
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <img src="/img/star.png" alt="" />
+                  <span>5</span>
+                </div>
+                <p>
+                  Amazing work! Communication was amazing, each and every day he
+                  sent me images that I was free to request changes to. They
+                  listened, understood, and delivered above and beyond my
+                  expectations. I absolutely recommend this gig, and know
+                  already that Ill be using it again very very soon
+                </p>
+                <div className="helpful">
+                  <span>Helpful?</span>
+                  <img src="/img/like.png" alt="" />
+                  <span>Yes</span>
+                  <img src="/img/dislike.png" alt="" />
+                  <span>No</span>
+                </div>
               </div>
-              <div className="stars">
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <img src="/img/star.png" alt="" />
-                <span>5</span>
+            </div>
+          </div>
+          <div className="right">
+            <div className="price">
+              <h3>{item.title}</h3>
+              <h2> TND {item.price}</h2>
+            </div>
+            <p>{item.description}</p>
+            <div className="details">
+              <div className="item">
+                <img src="/img/clock.png" alt="" />
+                <span>2 Days Delivery</span>
               </div>
-              <p>
-                Amazing work! Communication was amazing, each and every day he
-                sent me images that I was free to request changes to. They
-                listened, understood, and delivered above and beyond my
-                expectations. I absolutely recommend this gig, and know already
-                that Ill be using it again very very soon
-              </p>
-              <div className="helpful">
-                <span>Helpful?</span>
-                <img src="/img/like.png" alt="" />
-                <span>Yes</span>
-                <img src="/img/dislike.png" alt="" />
-                <span>No</span>
+              <div className="item">
+                <img src="/img/recycle.png" alt="" />
+                <span> {new Date(item.date).toLocaleDateString()} </span>
               </div>
             </div>
-          </div>
-        </div>
-        <div className="right">
-          <div className="price">
-            <h3>{item.title}</h3>
-            <h2> TND {item.price}</h2>
-          </div>
-          <p>{item.description}</p>
-          <div className="details">
-            <div className="item">
-              <img src="/img/clock.png" alt="" />
-              <span>2 Days Delivery</span>
+            <div className="features">
+              <div className="item">
+                <img src="/img/greencheck.png" alt="" />
+                <span>Arrivee {item.destination}</span>
+              </div>
+              <div className="item">
+                <img src="/img/greencheck.png" alt="" />
+                <span>Depart {item.depart}</span>
+              </div>
             </div>
-            <div className="item">
-              <img src="/img/recycle.png" alt="" />
-              <span> {new Date(item.date).toLocaleDateString()} </span>
-            </div>
+            <button onClick={submitOffer}>Continue</button>
           </div>
-          <div className="features">
-            <div className="item">
-              <img src="/img/greencheck.png" alt="" />
-              <span>Arrivee {item.destination}</span>
-            </div>
-            <div className="item">
-              <img src="/img/greencheck.png" alt="" />
-              <span>Depart {item.depart}</span>
-            </div>
-          </div>
-          <button onClick={submitOffer}>Continue</button>
         </div>
       </div>
     </div>
